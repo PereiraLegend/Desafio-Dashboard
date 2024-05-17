aa709f2dd61ca1ef1bf559c9bd9e4464f45c63f0
Lucas <pereirapro123@gmail.com>
2024-05-17 17:27:30 -0300

    Commit Finalizando Frontend

--- desafio-dashboard-frontend/app/components/tabela.tsx
+++ desafio-dashboard-frontend/app/components/tabela.tsx
@@ -1,5 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Table, Thead, Tbody, Tr, Th, Td, Button, Input } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Input, Heading, useToast, ChakraProvider, Table, Thead, Tbody, Tr, Th, Td, Flex, IconButton, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, AlertDialog, AlertDialogBody, AlertDialogFooter, AlertDialogHeader, AlertDialogContent, AlertDialogOverlay } from '@chakra-ui/react';
+import { FaArrowDownAZ } from 'react-icons/fa6';
+import { FaTrash } from "react-icons/fa";
+import { MdEditSquare } from "react-icons/md";
+import { TbPlayerTrackNextFilled } from "react-icons/tb";
+import { TbPlayerTrackPrevFilled } from "react-icons/tb";
+import { IoIosAdd } from "react-icons/io";
 import axios from 'axios';
 import Link from 'next/link';
 
@@ -8,6 +14,17 @@ export default function Tabela() {
     const [filtro, setFiltro] = useState('');
     const [dadosFiltrados, setDadosFiltrados] = useState([]);
     const [ordenacao, setOrdenacao] = useState({ coluna: null, ascendente: true });
+    const [isOpen, setIsOpen] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
+    const cancelRef = React.useRef();
+    const toast = useToast();
+    const { isOpen: isModalOpen, onOpen: onModalOpen, onClose: onModalClose } = useDisclosure();
+    const { isOpen: isEditModalOpen, onOpen: onEditModalOpen, onClose: onEditModalClose } = useDisclosure();
+    const [nome, setNome] = useState('');
+    const [cargo, setCargo] = useState('');
+    const [departamento, setDepartamento] = useState('');
+    const [selecionarEmpregado, setSelecionarEmpregado] = useState(null);
+    const [atualPage, setAtualPage] = useState(1);
 
     useEffect(() => {
         axios.get('http://localhost:5001/api/employees')
@@ -17,26 +34,52 @@ export default function Tabela() {
             })
             .catch(error => {
                 console.error('Erro ao buscar dados da API:', error);
+                toast({
+                    title: "Erro ao buscar dados da API.",
+                    description: error.message,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
             });
-    }, []);
+    }, [toast]);
 
-    const btnAlterar = (id) => {
-        console.log(`Alterar item com ID ${id}`);
+    const btnAlterar = (empregado) => {
+        setSelecionarEmpregado(empregado);
+        setNome(empregado.nome);
+        setCargo(empregado.cargo);
+        setDepartamento(empregado.departamento);
+        onEditModalOpen();
     };
 
     const btnDeletar = (id) => {
-        console.log("O id é :"+ id)
         axios.delete(`http://localhost:5001/api/employees/${id}`)
             .then(response => {
-                setDadosFiltrados(prevData => prevData.filter(item => item.id !== id));
-                console.log(`Item com ID ${id} deletado com sucesso!`);
+                setDadosFiltrados(prevData => prevData.filter(item => item._id !== id));
+                toast({
+                    title: `Item com ID ${id} deletado com sucesso!`,
+                    status: "success",
+                    duration: 5000,
+                    isClosable: true,
+                });
             })
             .catch(error => {
                 console.error(`Erro ao deletar item com ID ${id}:`, error);
+                toast({
+                    title: `Erro ao deletar item com ID ${id}.`,
+                    description: error.message,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
             });
-        window.location.reload()
+        setIsOpen(false);
     };
-    
+
+    const btnDelete = (id) => {
+        setItemToDelete(id);
+        setIsOpen(true);
+    };
 
     const handleFiltroChange = (event) => {
         const valorFiltro = event.target.value;
@@ -49,19 +92,15 @@ export default function Tabela() {
         setDadosFiltrados(dadosFiltrados);
     };
 
-    const handleOrdenacao = (coluna) => {
+    const funcOrdenacao = (coluna) => {
         setOrdenacao(prevState => ({
             coluna,
             ascendente: prevState.coluna === coluna ? !prevState.ascendente : true
         }));
 
         const ordenarPor = (a, b) => {
-            if (a[coluna] < b[coluna]) {
-                return -1;
-            }
-            if (a[coluna] > b[coluna]) {
-                return 1;
-            }
+            if (a[coluna] < b[coluna]) return -1;
+            if (a[coluna] > b[coluna]) return 1;
             return 0;
         };
 
@@ -73,60 +112,255 @@ export default function Tabela() {
         setDadosFiltrados(dadosOrdenados);
     };
 
+    const EnviarFormulario = async (e) => {
+        e.preventDefault();
+
+        const empregadoData = {
+            nome,
+            cargo,
+            departamento
+        };
+
+        try {
+            const response = await fetch('http://localhost:5001/api/employees', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(empregadoData)
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                setDados([...dados, result]);
+                setDadosFiltrados([...dados, result]);
+                onModalClose();
+                setNome('');
+                setCargo('');
+                setDepartamento('');
+                toast({
+                    title: 'Cadastro realizado com sucesso!',
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            } else {
+                toast({
+                    title: 'Erro ao cadastrar.',
+                    description: 'Por favor, tente novamente.',
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            toast({
+                title: 'Erro ao cadastrar.',
+                description: 'Por favor, tente novamente.',
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+
+    const EnviarFormularioAlterar = async (e) => {
+        e.preventDefault();
+
+        const empregadoData = {
+            nome,
+            cargo,
+            departamento
+        };
+
+        try {
+            const response = await fetch(`http://localhost:5001/api/employees/${selecionarEmpregado._id}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(empregadoData)
+            });
+
+            if (response.ok) {
+                const result = await response.json();
+                setDados(dados.map(emp => (emp._id === result._id ? result : emp)));
+                setDadosFiltrados(dadosFiltrados.map(emp => (emp._id === result._id ? result : emp)));
+                onEditModalClose();
+                setSelecionarEmpregado(null);
+                setNome('');
+                setCargo('');
+                setDepartamento('');
+                toast({
+                    title: 'Dados atualizados com sucesso!',
+                    status: 'success',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            } else {
+                toast({
+                    title: 'Erro ao atualizar.',
+                    description: 'Por favor, tente novamente.',
+                    status: 'error',
+                    duration: 5000,
+                    isClosable: true,
+                });
+            }
+        } catch (error) {
+            console.error('Erro:', error);
+            toast({
+                title: 'Erro ao atualizar.',
+                description: 'Por favor, tente novamente.',
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+            });
+        }
+    };
+
+    // Dividir os dados em lotes de 10 itens
+    const itemsPorPage = 10;
+    const totalPages = Math.ceil(dadosFiltrados.length / itemsPorPage);
+
+    // Função para obter os dados a serem exibidos na página atual
+    const getPaginatedData = () => {
+        const inicial = (atualPage - 1) * itemsPorPage;
+        const final = inicial + itemsPorPage;
+        return dadosFiltrados.slice(inicial, final);
+    };
+
+    // Função para avançar para a próxima página
+    const proximaPage = () => {
+        setAtualPage(anteriorPage => Math.min(anteriorPage + 1, totalPages));
+    };
+
+    // Função para voltar para a página anterior
+    const anteriorPage = () => {
+        setAtualPage(anteriorPage => Math.max(anteriorPage - 1, 1));
+    };
+
+    const numeroLinha = (index) => {
+        return (atualPage - 1) * itemsPorPage + index + 1;
+    };
+
     return (
-        <div>
-            <div className='pl-5 pr-5 flex justify-between '>
-                <Input
-                    className='border-4 rounded-lg'
-                    type="text"
-                    placeholder="Buscar..."
-                    value={filtro}
-                    onChange={handleFiltroChange}
-                    variant="filled"
-                    size="md"
-                    mb={4}
-                />
-
-                <Button colorScheme='teal' size='md' className='bg-green-200 hover:bg-green-300 p-1 rounded-lg'>
-                    <Link href="/cadastro">
-                        Cadastrar Funcionário
-                    </Link>
-                </Button>
-            </div>
-
-            <div className='flex w-[100%] justify-center items-center pl-5 pr-5'>
-                <Table variant="simple" size="md" className='w-[100%] min-w-full divide-y divide-gray-200'>
-                    <Thead>
-                        <Tr>
-                            <Th></Th>
-                            <Th onClick={() => handleOrdenacao('nome')} className="px-6 py-3 text-left text-6lg font-medium text-gray-500 uppercase tracking-wider cursor-pointer">Nome</Th>
-                            <Th onClick={() => handleOrdenacao('cargo')} className="px-6 py-3 text-left text-6lg font-medium text-gray-500 uppercase tracking-wider cursor-pointer">Cargo</Th>
-                            <Th onClick={() => handleOrdenacao('departamento')} className="px-6 py-3 text-left text-6lg font-medium text-gray-500 uppercase tracking-wider cursor-pointer">Departamento</Th>
-                            <Th className="px-6 py-3 text-left text-6lg font-medium text-gray-500 uppercase tracking-wider">Ações</Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                        {dadosFiltrados.length > 0 ? (
-                            dadosFiltrados.map((item, index) => (
-                                <Tr className='rounded-lg hover:bg-gray-200 hover:cursor-pointer' key={item.id}>
-                                    <Td className='p-1'>{index + 1}</Td>
-                                    <Td className="px-5 py-2 whitespace-nowrap">{item.nome}</Td>
-                                    <Td className="px-5 py-2 whitespace-nowrap">{item.cargo}</Td>
-                                    <Td className="px-5 py-2 whitespace-nowrap">{item.departamento}</Td>
-                                    <Td className="px-5 py-2 whitespace-nowrap">
-                                        <Button className="bg-blue-200 hover:bg-blue-300 p-1 rounded-lg mr-2" colorScheme="blue" onClick={() => btnAlterar(item.id)}>Alterar</Button>
-                                        <Button className="bg-red-200 hover:bg-red-300 p-1 rounded-lg" colorScheme="red" onClick={() => btnDeletar(item._id)}>Deletar</Button>
+        <ChakraProvider>
+            <Box p={5}>
+                <Flex justify="space-between" mb={4}>
+                    <Flex>
+                        <Input type="text" placeholder="Buscar..." className='border' value={filtro} onChange={handleFiltroChange} variant="filled" size="md" mr={2}/>
+                        <IconButton icon={<FaArrowDownAZ />} colorScheme="teal" onClick={() => funcOrdenacao('nome')}/>
+                    </Flex>
+                    <Button colorScheme="green" onClick={onModalOpen}><IoIosAdd/> Cadastrar Funcionário</Button>
+                </Flex>
+                <Box overflowX="auto">
+                    <Table variant="simple" size="md">
+                        <Thead>
+                            <Tr>
+                                <Th>#</Th>
+                                <Th onClick={() => funcOrdenacao('nome')} cursor="pointer">Nome</Th>
+                                <Th onClick={() => funcOrdenacao('cargo')} cursor="pointer">Cargo</Th>
+                                <Th onClick={() => funcOrdenacao('departamento')} cursor="pointer">Departamento</Th>
+                                <Th>Ações</Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {getPaginatedData().map((item, index) => (
+                                <Tr key={item._id} _hover={{ bg: "gray.100" }}>
+                                    <Td>{numeroLinha(index)}</Td>
+                                    <Td>{item.nome}</Td>
+                                    <Td>{item.cargo}</Td>
+                                    <Td>{item.departamento}</Td>
+                                    <Td>
+                                        <Button colorScheme="blue" size="sm" mr={2} title="Editar" onClick={() => btnAlterar(item)}><MdEditSquare /></Button>
+                                        <Button colorScheme="red" size="sm" title="Deletar" onClick={() => btnDelete(item._id)}><FaTrash /></Button>
                                     </Td>
                                 </Tr>
-                            ))
-                        ) : (
-                            <Tr>
-                                <Td colSpan="5">Carregando...</Td>
-                            </Tr>
-                        )}
-                    </Tbody>
-                </Table>
-            </div>
-        </div>
+                            ))}
+                        </Tbody>
+                    </Table>
+                </Box>
+
+                {/* Botões de navegação para páginas */}
+                <Flex justify="center" mt={4}>
+                    <Button mr={2} onClick={anteriorPage} disabled={atualPage === 1}><TbPlayerTrackPrevFilled /></Button>
+
+                    {Array.from({ length: totalPages }, (_, i) => (
+                        <Button key={i + 1} mr={2} onClick={() => setAtualPage(i + 1)} colorScheme={atualPage === i + 1 ? "teal" : "gray"}>{i + 1}</Button>
+                    ))}
+                    <Button onClick={proximaPage} disabled={atualPage === totalPages}><TbPlayerTrackNextFilled /></Button>
+                </Flex>
+
+                <AlertDialog isOpen={isOpen} leastDestructiveRef={cancelRef} onClose={() => setIsOpen(false)}>
+                    <AlertDialogOverlay>
+                        <AlertDialogContent>
+                            <AlertDialogHeader fontSize="lg" fontWeight="bold">Deletar Funcionário</AlertDialogHeader>
+                            <AlertDialogBody>Você tem certeza que deseja deletar este funcionário? Esta ação não pode ser desfeita.</AlertDialogBody>
+                            <AlertDialogFooter>
+                                <Button ref={cancelRef} onClick={() => setIsOpen(false)}>Cancelar</Button>
+                                <Button colorScheme="red" onClick={() => btnDeletar(itemToDelete)} ml={3}>Deletar</Button>
+                            </AlertDialogFooter>
+                        </AlertDialogContent>
+                    </AlertDialogOverlay>
+                </AlertDialog>
+
+                <Modal isOpen={isModalOpen} onClose={onModalClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Cadastrar Funcionário</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <form onSubmit={EnviarFormulario}>
+                                <FormControl id="nome" isRequired mb={4}>
+                                    <FormLabel>Nome</FormLabel>
+                                    <Input type="text" value={nome} onChange={(e) => setNome(e.target.value)}/>
+                                </FormControl>
+                                <FormControl id="cargo" isRequired mb={4}>
+                                    <FormLabel>Cargo</FormLabel>
+                                    <Input type="text" value={cargo} onChange={(e) => setCargo(e.target.value)}/>
+                                </FormControl>
+                                <FormControl id="departamento" isRequired mb={4}>
+                                    <FormLabel>Departamento</FormLabel>
+                                    <Input type="text" value={departamento} onChange={(e) => setDepartamento(e.target.value)}/>
+                                </FormControl>
+                                <Button type="submit" colorScheme="blue" width="full" mt={4}>Cadastrar</Button>
+                            </form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onModalClose} width="full">Fechar</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+
+                <Modal isOpen={isEditModalOpen} onClose={onEditModalClose}>
+                    <ModalOverlay />
+                    <ModalContent>
+                        <ModalHeader>Alterar Funcionário</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody>
+                            <form onSubmit={EnviarFormularioAlterar}>
+                                <FormControl id="nome" isRequired mb={4}>
+                                    <FormLabel>Nome</FormLabel>
+                                    <Input type="text" value={nome} onChange={(e) => setNome(e.target.value)}/>
+                                </FormControl>
+                                <FormControl id="cargo" isRequired mb={4}>
+                                    <FormLabel>Cargo</FormLabel>
+                                    <Input type="text" value={cargo} onChange={(e) => setCargo(e.target.value)}/>
+                                </FormControl>
+                                <FormControl id="departamento" isRequired mb={4}>
+                                    <FormLabel>Departamento</FormLabel>
+                                    <Input type="text" value={departamento} onChange={(e) => setDepartamento(e.target.value)}/>
+                                </FormControl>
+                                <Button type="submit" colorScheme="blue" width="full" mt={4}>Alterar</Button>
+                            </form>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button onClick={onEditModalClose} width="full">Fechar</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </Modal>
+            </Box>
+        </ChakraProvider>
     );
 }
